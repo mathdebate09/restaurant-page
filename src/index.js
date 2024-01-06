f3933e54eeb52939d92ba3fe051f3a62adb94093
Jay Singh <learn.jayssingh@gmail.com>
2024-01-06 12:58:53 +0530

    webpack build index.html dislocated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("this works");
+import './styles.css';
+import Icon from './assets/icon.svg';
+
+const content = document.querySelector('#content');
+
+const pizza = document.createElement('div');
+pizza.innerHTML = `<img src="${Icon}">`;
+pizza.classList.add('pizza');
+
+content.appendChild(pizza);
